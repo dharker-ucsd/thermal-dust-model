1de7f4d5d449e0e1bd3bd9430c653250060bef9e
David E. Harker <dharker@ucsd.edu>
2017-03-14 08:10:06 -0700

    Clean up

--- dust/__init__.py
+++ dust/__init__.py
@@ -25,32 +25,3 @@ from .materials import *
 from .model import *
 from .results import *
 from .util import *
-
-########################################################################
-def _configure():
-    """Default configuration: read or create config file."""
-    import os
-    import configparser
-
-    config = configparser.ConfigParser()
-    config_file = os.sep.join((os.environ['HOME'], '.config', 'dust', 'config.ini'))
-    if os.path.exists(config_file):
-        config.read(config_file)
-    else:
-        path = os.sep.join((os.environ['HOME'], 'Projects', 'src', 'thermal-dust-model', 'data'))
-        config['fit-idl-save'] = {'Path': path}
-        del path
-
-        i = 0
-        while i >= 0:
-            if i != 0:  # no need to create a root directory!
-                if not os.path.isdir(config_file[:i]):
-                    os.mkdir(config_file[:i])
-            i = config_file.find(os.sep, i + 1)
-
-        with open(config_file, 'w') as outf:
-            config.write(outf)
-
-    return config
-
-_config = _configure()
