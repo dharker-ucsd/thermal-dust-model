492dc1070514271ebcd6334aef056705f162a664
David E. Harker <dharker@ucsd.edu>
2017-04-17 13:10:34 -0700

    Fix a bug in fit-idl-save.py

--- scripts/fit-idl-save.py
+++ scripts/fit-idl-save.py
@@ -137,16 +137,15 @@ assert any(i), 'Error: None of D={} in {}'.format(args.D, Ds)
 
 #### This is hard coded to assume the first three materials are amorphous and
 #### the last ones are crystalline.  CHANGED BELOW
-for j in range(3):  # amorphous dust
-    mfluxd[j] = mfluxd[j][:, :, i]
-
-for j in range(3, len(files)):   # crystalline dust
-    mfluxd[j] = np.repeat(mfluxd[j], len(Ds), 2) 
+#for j in range(3):  # amorphous dust
+#    mfluxd[j] = mfluxd[j][:, :, i]
+#for j in range(3, len(files)):   # crystalline dust
+#    mfluxd[j] = np.repeat(mfluxd[j], len(Ds), 2) 
 
 # Given the nature of the IDL save files, amorphous and crystalline materials
 # are treated differently.  But now the order should be irrelevant.
-for j in range(len(files)):  
-    if material_classes[0]().mtype.startswith('a'):
+for j in range(len(files)):
+    if material_classes[j]().mtype.startswith('a'):
         mfluxd[j] = mfluxd[j][:, :, i]  # amorphous dust
     else:
         mfluxd[j] = np.repeat(mfluxd[j], len(Ds), 2) # crystalline dust
